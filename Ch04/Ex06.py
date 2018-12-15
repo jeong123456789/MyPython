6bb54114760bf6f930d2297631df60a829b9531d
jeong123456789 <45708165+jeong123456789@users.noreply.github.com>
2018-12-15 16:57:07 +0900

    2018-12-15
    2018-12-15

---
+++ Ch04/Ex06.py
@@ -0,0 +1,8 @@
+
+height = float(input("키를 미터 단위로 입력하세요: ")
+weight = float(input("몸무게를 kg 단위로 입력하세요: ")
+
+
+bmi = weight/height**2
+
+print("당신의 bmi는",bmi,"입니다.")               
---
+++ Ch04/p05.py
@@ -0,0 +1,2 @@
+s = "hello Python"
+print(s[3:7])
